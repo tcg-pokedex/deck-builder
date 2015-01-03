4600598ae26d30ebb2afcc4b971788b13309fbe6
kristenmills <kristen@kristen-mills.com>
2015-01-03 12:15:08 -0800

    Added the pokemon card element

---
+++ src/elements/pokemon-card/pokemon-card.js
@@ -0,0 +1,12 @@
+Polymer({
+  computed: {
+    name: "pokemon.set + '_'+ pad(pokemon.number, 3)",
+    src: "'http://www.lackeyccg.com/pokemon/high/cards/' + name + '.jpg'"
+  },
+  pad: function(n, p, c) {
+    var n = parseInt(this.pokemon.number);
+    var pad_char = typeof c !== 'undefined' ? c : '0';
+    var pad = new Array(1 + p).join(pad_char);
+    return (pad + n).slice(-pad.length);
+  },
+});
